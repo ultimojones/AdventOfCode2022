67a04adda5bc91983531d9aaa7b0fa17c7f3bede
Bert Lyons <ultimojones@gmail.com>
2022-12-09 08:49:32 +1100

    🤠

--- AdventOfCode2022/Day8/Program.cs
+++ AdventOfCode2022/Day8/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Day8
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Day8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Part1();
+            Part2();
+        }
+
+        private static void Part2()
+        {
+            var grid = File.ReadAllLines("input.txt")
+                .SelectMany((l, x) => l.Select((c, y) => new { x, y, h = int.Parse(c.ToString()) }))
+                .ToDictionary(a => (a.x, a.y), a => (a.h, v: 0));
+
+            for (int x = 1; x < 98; x++)
+            {
+                for (int y = 1; y < 98; y++)
+                {
+                    int l, r, u, d;
+                    for (l = 1; l < y; l++) { if (grid[(x, y - l)].h >= grid[(x, y)].h) break; }
+                    for (r = 1; r < 98 - y; r++) { if (grid[(x, y + r)].h >= grid[(x, y)].h) break; }
+                    for (u = 1; u < x; u++) { if (grid[(x - u, y)].h >= grid[(x, y)].h) break; }
+                    for (d = 1; d < 98 - x; d++) { if (grid[(x + d, y)].h >= grid[(x, y)].h) break; }
+                    grid[(x, y)] = (grid[(x, y)].h, l * r * u * d);
+                }
+            }
+
+            Console.WriteLine(grid.Max(x => x.Value.v));
         }
 
         private static void Part1()
