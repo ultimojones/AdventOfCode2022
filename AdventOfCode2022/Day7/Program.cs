8d02c62bbfbae4798dc4a706e2c8c041675d6c9f
Bert Lyons <ultimojones@gmail.com>
2022-12-08 21:13:51 +1100

    🤠

---
+++ AdventOfCode2022/Day7/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace Day7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part2();
+        }
+
+        private static void Part2()
+        {
+            using var input = File.OpenText("input.txt");
+            var line = input.ReadLine();
+
+            var path = new Stack<string>();
+            string dir = "";
+            var files = new Dictionary<string, int>();
+
+            while (line != null)
+            {
+                if (line[0] == '$')
+                {
+                    switch (line[2..4])
+                    {
+                        case "cd":
+                            switch (line[5..])
+                            {
+                                case "/":
+                                    path.Clear();
+                                    break;
+                                case "..":
+                                    path.Pop();
+                                    break;
+                                default:
+                                    path.Push(line[5..]);
+                                    break;
+                            }
+                            dir = '/' + String.Join('/', path.Reverse());
+                            files.TryAdd(dir, 0);
+                            break;
+                        case "ls":
+                            break;
+                        default:
+                            throw new InvalidOperationException();
+                    }
+                }
+                else if (char.IsDigit(line[0]))
+                {
+                    var parts = line.Split(' ');
+                    var len = int.Parse(parts[0]);
+                    var file = parts[1];
+                    var cur = files[dir];
+                    files[dir] = cur + len;
+                }
+
+                line = input.ReadLine();
+            }
+
+            var totals = new Dictionary<string, int>();
+
+            foreach (var item in files)
+            {
+                var sum = files.Where(p => p.Key.StartsWith(item.Key)).Sum(p => p.Value);
+                totals[item.Key] = sum;
+            }
+
+            foreach (var item in totals.Where(t => t.Value <= 100000))
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+
+            Console.WriteLine(totals.Where(t => t.Value <= 100000).Sum(t => t.Value));
+
+            // 48518336
+            // 21481664
+            //Console.WriteLine(totals.Where(t => t.Value >= 70000000 - totals["/"])
+            //    .OrderBy(t => t.Value).First().Value);
+
+            var minSize = 30000000 - (70000000 - totals["/"]);
+            var candidates = totals.Where(t => t.Value >= minSize).OrderBy(t => t.Value).ToList();
+            Console.WriteLine(candidates.First());
+        }
+
+        private static void Part1()
+        {
+            using var input = File.OpenText("input.txt");
+            var line = input.ReadLine();
+
+            var path = new Stack<string>();
+            string dir = "";
+            var files = new Dictionary<string, int>();
+
+            while (line != null)
+            {
+                if (line[0] == '$')
+                {
+                    switch (line[2..4])
+                    {
+                        case "cd":
+                            switch (line[5..])
+                            {
+                                case "/":
+                                    path.Clear();
+                                    break;
+                                case "..":
+                                    path.Pop();
+                                    break;
+                                default:
+                                    path.Push(line[5..]);
+                                    break;
+                            }
+                            dir = '/' + String.Join('/', path.Reverse());
+                            files.TryAdd(dir, 0);
+                            break;
+                        case "ls":
+                            break;
+                        default:
+                            throw new InvalidOperationException();
+                    }
+                }
+                else if (char.IsDigit(line[0]))
+                {
+                    var parts = line.Split(' ');
+                    var len = int.Parse(parts[0]);
+                    var file = parts[1];
+                    var cur = files[dir];
+                    files[dir] = cur + len;
+                }
+
+                line = input.ReadLine();
+            }
+
+            var totals = new Dictionary<string, int>();
+
+            foreach (var item in files)
+            {
+                var sum = files.Where(p => p.Key.StartsWith(item.Key)).Sum(p => p.Value);
+                totals[item.Key] = sum;
+            }
+
+            foreach (var item in totals.Where(t => t.Value <= 100000))
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+
+            Console.WriteLine(totals.Where(t => t.Value <= 100000).Sum(t => t.Value));
+        }
+    }
+}
---
+++ AdventOfCode2022/Day8/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Day8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+
+        private static void Part1()
+        {
+            var grid = LoadGrid();
+            int count = 0;
+            for (int i = 0; i < 99; i++)
+            {
+                for (int j = 0; j < 99; j++)
+                {
+                    var tree = grid[i, j];
+                    var up = Enumerable.Range(0, j).All(x => grid[i, x].height < tree.height);
+                    var down = Enumerable.Range(j + 1, 98 - j).All(x => grid[i, x].height < tree.height);
+                    var left = Enumerable.Range(0, i).All(x => grid[x, j].height < tree.height);
+                    var right = Enumerable.Range(i + 1, 98 - i).All(x => grid[x, j].height < tree.height);
+                    if (grid[i, j].visible = up || down || left || right)
+                        count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        private static (int height, bool visible)[,] LoadGrid()
+        {
+            var grid = new (int, bool)[99, 99];
+            using var input = File.OpenText("input.txt");
+            var line = input.ReadLine();
+            int count = 0;
+            while (line != null)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    grid[count, i] = (int.Parse(line[i].ToString()), false);
+                }
+                count++;
+                line = input.ReadLine();
+            }
+            return grid;
+        }
+    }
+}
