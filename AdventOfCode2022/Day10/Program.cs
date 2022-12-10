7689c67e6dc8e26ea7cd41f9cdbb25ab5d90d4e9
Bert Lyons <ultimojones@gmail.com>
2022-12-10 18:10:40 +1100

    🤠

--- AdventOfCode2022/Day10/Program.cs
+++ AdventOfCode2022/Day10/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Part2();
+        }
+
+        private static void Part2()
+        {
+            var signal = 1;
+            var signals = new List<int>() { signal };
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                signals.Add(signal);
+                if (line.StartsWith("addx"))
+                {
+                    signals.Add(signal += int.Parse(line.Substring(5)));
+                }
+            }
+
+            for (int i = 0; i < 240; i++)
+            {
+                var pos = i % 40;
+                Console.Write(Math.Abs(signals[i] - pos) <= 1 ? '#' : '.');
+
+                if (pos == 39) Console.WriteLine();
+            }
+        }
+
+        private static void Part1()
+        {
+            var signal = 1;
+            var signals = new List<int>() { signal };
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                signals.Add(signal);
+                if (line.StartsWith("addx"))
+                {
+                    signals.Add(signal += int.Parse(line.Substring(5)));
+                }
+            }
+
+            int CalcStrength(int cycle) => cycle * signals![cycle - 1];
+            var total = CalcStrength(20) + CalcStrength(60) + CalcStrength(100) + CalcStrength(140) + CalcStrength(180) + CalcStrength(220);
+            Console.WriteLine(total);
         }
     }
 }
